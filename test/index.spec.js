71af9d260ea8777042d372779f2a8a30a9d820f8
Connor Wiseman <wiseman.connor@gmail.com>
2017-09-16 15:40:59 -0700

    .headers property

--- test/index.spec.js
+++ test/index.spec.js
@@ -13,21 +13,43 @@ const request = require('../lib/index.js');
 
 
 describe('request', () => {
+  let origReg;
+
+  beforeEach(() => {
+    origReq = {};
+  });
+
   it('should export a function', () => {
     request.should.be.a('Function');
   });
 
   it('should return an object', () => {
-    request({}).should.be.an('Object');
+    request(origReq).should.be.an('Object');
+  });
+
+  describe('`.headers`', () => {
+    it('should return an object', () => {
+      origReq.headers = {};
+      request(origReq).headers.should.be.an('Object');
+    });
+
+    it('should return the HTTP headers used by the original request', () => {
+      origReq.headers = {
+        test: true
+      };
+      request(origReq).headers.should.equal(origReq.headers);
+    });
   });
 
   describe('`.method`', () => {
-    it('should return the HTTP method used by the original request', () => {
-      let req = request({
-        method: 'test'
-      });
+    it('should return a string', () => {
+      origReq.method = 'test';
+      request(origReq).method.should.be.a('String');
+    });
 
-      req.method.should.equal('test');
+    it('should return the HTTP method used by the original request', () => {
+      origReq.method = 'test';
+      request(origReq).method.should.equal(origReq.method);
     });
   });
 });
